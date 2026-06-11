4de22182b5019a0f907b8edbc74a8f61e23892b4
Nicole Mayrhofer <s2510237918@students.fh.hagenberg.at>
2026-06-11 06:54:53 +0200

    last modified 3.6.26

--- app/src/main/java/at/fhooe/sail/mc/jpc/MainActivity.kt
+++ app/src/main/java/at/fhooe/sail/mc/jpc/MainActivity.kt
@@ -6,42 +6,67 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import at.fhooe.sail.mc.jpc.ui.theme.JpCTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+// set content ist einziger bereich wo ein composable implementiert werden kann
         setContent {
             JpCTutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "again"
                     )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+                          // companion-objekt vom typ modifier -> hier ohne ()
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
+        modifier = modifier,
         text = "Hello $name!",
-        modifier = modifier
-    )
+        color = MaterialTheme.colorScheme.primary,
+        style = MaterialTheme.typography.headlineMedium,
+        )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreviewA() {
     JpCTutorialTheme {
         Greeting("again")
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFF8BC34A)
+@Composable
+private fun GreetingPreviewB() {
+    JpCTutorialTheme() {
+        Greeting("Class")
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFC107)
+@Composable
+private fun GreetingPreviewC() {
+    JpCTutorialTheme() {
+        Greeting("Nicole")
     }
 }
